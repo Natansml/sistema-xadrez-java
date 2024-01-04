3195ab0fb5b312cdad948ace1721bc1eb0a22ae7
Natansml <natanmello@yahoo.com>
2024-01-04 18:32:31 -0300

    Adiciona classe Position

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("teste");
+        Position pos = new Position(3, 2);
+
+        System.out.println(pos);
     }
 }
